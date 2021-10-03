eeb778b1220f4443252187cc39c8185d6ce0c9a1
Dennis Bersenev <dennis_bersenev@live.com>
2021-10-03 15:44:30 -0700

    all done

--- a1/q3.cpp
+++ a1/q3.cpp
@@ -152,7 +152,7 @@ float sensor(int obs, state_t s)
 void output_beliefs()
 {
     FILE* fd;
-    if (fopen_s(&fd, "out/test_4", "w") != 0 || fd == NULL)
+    if (fopen_s(&fd, "out/test_3", "w") != 0 || fd == NULL)
         exit(errno);
 
     if (_dup2(_fileno(fd), _fileno(stdout)) != 0)
@@ -170,26 +170,25 @@ void output_beliefs()
 int main()
 {
     
-    int seq_length = 4;
+    int seq_length = 3;
 
     //1. Specify strat
     //init_belief_uniform();
-    //state_t s_initial{3, 2};
-    //init_belief(s_initial)
-    state_t s_initial{1, 1};
-    init_belief(s_initial)
+    state_t s_initial{3, 2};
+    //state_t s_initial{1, 1};
+    init_belief(s_initial);
     
     //2. Specify action seq
     //int actions[] = {0, 0, 0};
     //int actions[] = {0, 0, 0};
-    //int actions[] = {3, 3, 0};
-    int actions[] = {0, 3, 3, 3};
+    int actions[] = {3, 3, 0};
+    //int actions[] = {0, 3, 3, 3};
 
     //3. Specify observation seq
     //int obs[] = {2, 2, 2};
     //int obs[] = {1, 1, 1};
-    //int obs[] = {1, 1, 0};
-    int obs[] = {2, 2, 1, 1};
+    int obs[] = {1, 1, 0};
+    //int obs[] = {2, 2, 1, 1};
 
     //eg. test transition
     for (int i = 0; i < seq_length; i++) {
